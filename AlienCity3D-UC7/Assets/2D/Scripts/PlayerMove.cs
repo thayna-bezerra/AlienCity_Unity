e7a34410b8299dc3759903f5addce301496fc1f2
thayna-bezerra <arraisthayna2106@gmail.com>
2022-01-31 15:59:47 -0300

    move player e canvas

---
+++ AlienCity3D-UC7/Assets/2D/Scripts/PlayerMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMove : MonoBehaviour
+{
+    public Text textoRodape; 
+    
+    public Animator AnimacoesPlayer;
+
+    private void Start()
+    {
+        AnimacoesPlayer = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if(touch.phase == TouchPhase.Moved)
+            {
+                transform.position += (Vector3)touch.deltaPosition;
+                textoRodape.gameObject.SetActive(true);
+                AnimacoesPlayer.Play("Jump"); //usar bool isActive para verificar se o player está ou não sendo tocando, para assim tocar a anim
+            }
+
+            else { textoRodape.gameObject.SetActive(false); }
+        }
+    }
+}
