5cf78b43dae9d52044f9a466d619626eea66f9bd
thayna-bezerra <arraisthayna2106@gmail.com>
2022-02-04 15:36:04 -0300

    atv 2 - mov com joystick e dano das armadilhas

---
+++ AlienCity3D-UC7/Assets/3D/Scripts/Armadilhas.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Armadilhas : MonoBehaviour
+{
+    //publico:  tempo de dano e quantidade de dano
+
+    public PlayerController pc;
+
+    public float maxTimeToDamage = 1;
+    private float currentTimeToDamage = 0;
+    private bool isColliding = false;
+
+    void Start()
+    {
+        pc = FindObjectOfType<PlayerController>();
+    }
+
+    void Update()
+    {
+        if (isColliding)
+        {
+            if (currentTimeToDamage >= maxTimeToDamage)
+            {
+                DamagePlayer();
+                currentTimeToDamage = 0;
+            }
+            else
+                currentTimeToDamage += Time.deltaTime;
+        }
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isColliding = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isColliding = false;
+        }
+    }
+
+    void DamagePlayer()
+    {
+        pc.lifePlayer--;
+    }
+
+
+    /* public Slider vidaPlayer;
+     public float maxTimeToDamage = 1;
+     private float currentTimeToDamage = 0;
+     private bool isColliding = false;
+
+     void Update()
+     {
+         if (isColliding)
+         {
+             if (currentTimeToDamage >= maxTimeToDamage)
+             {
+                 DamagePlayer();
+                 currentTimeToDamage = 0;
+             }
+             else
+                 currentTimeToDamage += Time.deltaTime;
+         }
+     }
+
+     private void OnTriggerEnter(Collider other)
+     {
+         if (other.tag == "Player")
+         {
+             isColliding = true;
+         }
+
+
+     }
+
+     void OnTriggerExit(Collider other)
+     {
+         if (other.tag == "Player")
+         {
+             isColliding = false;
+         }
+     }
+
+     void DamagePlayer()
+     {
+         vidaPlayer.value--;
+     }
+ */
+}
---
+++ AlienCity3D-UC7/Assets/3D/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour 
+{
+	[Header("Move Player")] 
+	[Space]
+	public float MoveSpeed;
+	public float RotationSpeed;
+	CharacterController cc;
+	private Animator anim;
+	protected Vector3 gravidade = Vector3.zero;
+	protected Vector3 move = Vector3.zero;
+	private bool jump = false;
+	public Joystick joystick;
+
+	//[Space(10)]
+
+	public int lifePlayer = 5;
+
+
+	void Start()
+	{
+		cc = GetComponent<CharacterController> ();
+		anim = GetComponent<Animator>();
+		anim.SetTrigger("Parado");
+	}
+
+	void Update()
+	{
+		Vector3 a = Input.GetAxis ("Vertical") * transform.TransformDirection (Vector3.forward) * MoveSpeed;
+		transform.Rotate (new Vector3 (0, Input.GetAxis ("Horizontal") * RotationSpeed * Time.deltaTime, 0));
+
+
+		Vector3 move = joystick.Vertical * transform.TransformDirection (Vector3.forward) * MoveSpeed;
+		transform.Rotate (new Vector3 (0, joystick.Horizontal * RotationSpeed * Time.deltaTime, 0));
+		
+		if (!cc.isGrounded) {
+			gravidade += Physics.gravity * Time.deltaTime;
+		} 
+		else 
+		{
+			gravidade = Vector3.zero;
+			if(jump)
+			{
+				gravidade.y = 6f;
+				jump = false;
+			}
+		}
+		move += gravidade;
+		cc.Move (move* Time.deltaTime);
+		Anima ();
+	}
+	 
+	void Anima()
+	{
+		if (!Input.anyKey && Input.GetAxis("Vertical") == 0)
+		{
+			anim.SetTrigger("Parado");
+		} 
+
+		else { anim.SetTrigger("Corre"); }
+	}
+
+	public void btnJump()
+	{
+		anim.SetTrigger("Pula");
+		jump = true;
+	}
+}
